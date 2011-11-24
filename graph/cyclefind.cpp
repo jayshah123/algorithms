18a63c02c32e6c852b72022543dac3f03bada5b1
jay shah <competitor123@gmail.com>
2011-11-24 06:08:09 +0530

    cyclefind

---
+++ graph/cyclefind.cpp
@@ -0,0 +1,67 @@
+//cycle in graph
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+#define SZ 6
+
+int v[SZ];
+int p[SZ];
+int g[SZ][SZ]={{0,1,0,1,0,0},
+               {0,0,0,0,1,0},
+               {0,0,0,0,1,1},
+               {0,1,0,0,0,0},
+               {0,0,0,1,0,0},
+               {0,0,0,0,0,1}};
+               
+int printpath(int i)
+{
+  if(p[i]==i){
+           cout<<i;
+           return i;}
+  else{
+     cout<<i;
+     return printpath(p[i]);
+  }
+}
+
+void dfs(int u){
+if(v[u]) return;//if vertex already visited return
+
+v[u] = 1;//else visit the vertex
+cout << u;
+for(int i = 0 ; i < SZ ; i++)//check for
+if(g[u][i]&&v[i]&&i!=p[u])//adjacent except parent is visited -> cycle!
+{
+               cout<<endl<<"CYCLE RUN!"<<endl;
+}
+else if(g[u][i]&&!v[i])
+{
+            p[i]=u;
+            dfs(i);//call
+}
+
+}
+int main()
+{
+ 
+    
+// function calls
+for(int i = 0 ; i < SZ ; i++)
+      {  v[i] = 0;//no vertex has been visited
+         p[i]=0;
+      } 
+for(int i = 0 ; i < SZ ; i++)
+        if(!v[i]){
+                  p[i]=i;
+                dfs(i);
+        }
+cout<<endl;
+for(int i=0;i<6;i++)
+        cout<<endl<<p[i];
+
+cout<<endl<<endl;
+printpath(1);
+    system("pause");
+    return 0;
+}
